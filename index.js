86a674c35ed520c20a4419f51f47142412509088
Hariom Khomane <hariomkhomane23@gmail.com>
2026-08-09 22:31:51 +0530

    Update CRUD application

--- index.js
+++ index.js
@@ -4,13 +4,17 @@ const port = process.env.PORT || 8080;
 
 const path = require("path");
 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 
+// EJS setup
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+// Static files
 app.use(express.static(path.join(__dirname, "public")));
 
+// Posts data
 let posts = [
     {
         id: "1",
@@ -30,27 +34,27 @@ let posts = [
 ];
 
 
-// -------------------
+// ===============================
 // SHOW ALL POSTS
-// -------------------
+// ===============================
 
 app.get("/posts", (req, res) => {
     res.render("index.ejs", { posts });
 });
 
 
-// -------------------
+// ===============================
 // SHOW NEW POST FORM
-// -------------------
+// ===============================
 
 app.get("/posts/new", (req, res) => {
     res.render("new.ejs");
 });
 
 
-// -------------------
+// ===============================
 // CREATE NEW POST
-// -------------------
+// ===============================
 
 app.post("/posts", (req, res) => {
 
@@ -64,14 +68,14 @@ app.post("/posts", (req, res) => {
         content
     });
 
-    // Redirect to main page
+    // Go back to main page
     res.redirect("/posts");
 });
 
 
-// -------------------
+// ===============================
 // SHOW EDIT FORM
-// -------------------
+// ===============================
 
 app.get("/posts/:id/edit", (req, res) => {
 
@@ -79,13 +83,17 @@ app.get("/posts/:id/edit", (req, res) => {
 
     let post = posts.find((p) => p.id === id);
 
+    if (!post) {
+        return res.status(404).send("Post not found");
+    }
+
     res.render("edit.ejs", { post });
 });
 
 
-// -------------------
+// ===============================
 // UPDATE POST
-// -------------------
+// ===============================
 
 app.post("/posts/:id/edit", (req, res) => {
 
@@ -93,19 +101,23 @@ app.post("/posts/:id/edit", (req, res) => {
 
     let post = posts.find((p) => p.id === id);
 
+    if (!post) {
+        return res.status(404).send("Post not found");
+    }
+
     let { username, content } = req.body;
 
     post.username = username;
     post.content = content;
 
-    // Redirect to main page after editing
+    // Go back to main page
     res.redirect("/posts");
 });
 
 
-// -------------------
+// ===============================
 // DELETE POST
-// -------------------
+// ===============================
 
 app.post("/posts/:id/delete", (req, res) => {
 
@@ -113,15 +125,15 @@ app.post("/posts/:id/delete", (req, res) => {
 
     posts = posts.filter((p) => p.id !== id);
 
-    // Redirect to main page after deleting
+    // Go back to main page
     res.redirect("/posts");
 });
 
 
-// -------------------
+// ===============================
 // START SERVER
-// -------------------
+// ===============================
 
 app.listen(port, () => {
-    console.log("listening to port : 8080");
+    console.log(`Server running on port ${port}`);
 });
